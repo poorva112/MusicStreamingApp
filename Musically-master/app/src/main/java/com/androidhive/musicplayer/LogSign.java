fbf406549a6b91d19bc47ea6c5cae84ba1fd7dc1
Poorva <college@Poorvas-MacBook-Pro.local>
2019-04-07 14:18:35 +0530

    App

---
+++ Musically-master/app/src/main/java/com/androidhive/musicplayer/LogSign.java
@@ -0,0 +1,16 @@
+package com.androidhive.musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LogSign extends AppCompatActivity {
+
+    Button Login, Register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_sign);
+
+    }
+}
