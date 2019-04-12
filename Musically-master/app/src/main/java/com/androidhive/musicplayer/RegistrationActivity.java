eeb981adeaf767884878175378751b6756d95fba
Poorva <poorvatiwari112@gmail.com>
2019-04-13 01:54:52 +0530

    Authentication added

--- Musically-master/app/src/main/java/com/androidhive/musicplayer/RegistrationActivity.java
+++ Musically-master/app/src/main/java/com/androidhive/musicplayer/RegistrationActivity.java
@@ -2,6 +2,7 @@ package com.androidhive.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -19,5 +20,26 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        initializeUI();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerNewUser();
+            }
+        });
+    }
+
+
+    private void initializeUI() {
+        emailTV = (EditText) findViewById(R.id.email);
+        passwordTV = (EditText) findViewById(R.id.password1);
+
+
+        regBtn = (Button) findViewById(R.id.register);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
 }
